d8638d0553a5f4d3629b88f78fdd4ec16a6ca100
MSI\kamad <zeal23300159@fsgcl.jp>
2025-08-23 01:56:15 +0900

    コマンドコード書き換え

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,5 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server running');
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));
